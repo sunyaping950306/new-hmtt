848bc705bf6dca7292bab1daa58f3ade01e1fa7a
sunyaping950306 <1250823197@qq.com>
2019-08-25 20:51:36 +0800

    使用vue-router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 导包
+import ElementUI from 'element-ui'
+// 导入路由文件
+import router from '@/router'
+
+// 引入样式
+import 'element-ui/lib/theme-chalk/index.css'
+// 在全局范围内注册element-ui组件库
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+// 创建路由对象、配置路由规则、配置其他路由
+// 导入vue
+import Vue from 'vue'
+// 导入vue-router
+import VueRouter from 'vue-router'
+// 注册vue-router
+Vue.use(VueRouter)
+// 初始化router对象(配置路由规则)
+const router = new VueRouter({
+  // 路由规则
+  routes: []
+})
+// 导出、挂载到main.js根实例下
+export default router
