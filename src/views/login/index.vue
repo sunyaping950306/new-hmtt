d4bcba25ba6a5e88898dc5d653ec4cca9015371c
sunyaping950306 <1250823197@qq.com>
2019-08-26 16:35:47 +0800

    登录模块-进行登录

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,12 +4,12 @@
     <el-card class="login_box">
       <img src="../../assets/images/logo_index.png" alt />
       <!-- 表单组件 -->
-      <el-form>
-        <el-form-item>
-          <el-input placeholder="请输入手机号"></el-input>
+      <el-form :model="loginForm" :rules="rules" status-icon ref="loginForm">
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input placeholder="请输入验证码" style="width:280px"></el-input>
+        <el-form-item prop="code">
+          <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:280px"></el-input>
           <el-button style="float:right">发送验证码</el-button>
         </el-form-item>
         <el-form-item>
@@ -18,7 +18,7 @@
           <el-link type="primary">隐私条款</el-link>
         </el-form-item>
         <el-form-item>
-          <el-button style="width:100%" type="primary">登 录</el-button>
+          <el-button style="width:100%" type="primary" @click="login()">登 录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -26,7 +26,64 @@
 </template>
 
 <script>
-export default {}
+export default {
+  // 表单数据需要提交，绑定表单数据
+  data () {
+    // 自定义检验，在return前声明函数
+    const checkMobile = (rule, value, callback) => {
+      // 校验逻辑
+      if (/^1[3-9]\d{9}$/.test(value)) {
+        callback()
+      } else {
+        callback(new Error('手机号格式不对'))
+      }
+    }
+    return {
+      // 表单对应的对象
+      loginForm: {
+        mobile: '13911111111',
+        code: '246810'
+      },
+      // 默认勾选"我已阅读并同意"
+      checked: true,
+      // Form组件通过rules属性传入约定的验证规则
+      // Form-Item 的 prop 属性设置为需校验的字段名
+      rules: {
+        mobile: [
+          // 具体的校验规则 比如是否必填 长度 格式
+          { required: true, message: '手机号必填', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '验证码必填', trigger: 'blur' },
+          { len: 6, message: '验证码为6位', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    login () {
+    // 整体表单的校验
+      this.$refs.loginForm.validate((valid) => {
+        if (valid) {
+        // 如果校验成功，进行登录
+          this.$http.post('http://ttapi.research.itcast.cn/mp/v1_0/authorizations', 'this.loginForm')
+            .then(res => {
+              // res是响应对象 包含响应数据
+              const data = res.data
+              // 后台返回的json内容 已经转换成了对象
+              console.log(data)
+            //   登录成功后做什么事情？？
+            })
+            .catch(() => {
+              // 提示错误 使用组件 消息提示组件
+              this.$message.error('用户名或密码错误')
+            })
+        }
+      })
+    }
+  }
+}
 </script>
 
 <style lang="less">
