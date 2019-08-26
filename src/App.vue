2e137a98bf352d7e66d09e4def883aad6dc62b78
sunyaping950306 <1250823197@qq.com>
2019-08-26 10:38:00 +0800

    完成登录卡片

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <!-- 一级路由渲染的位置 -->
+    <router-view>login</router-view>
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,15 @@
 import Vue from 'vue'
 // 导入vue-router
 import VueRouter from 'vue-router'
+import Login from '@/views/login'
 // 注册vue-router
 Vue.use(VueRouter)
 // 初始化router对象(配置路由规则)
 const router = new VueRouter({
   // 路由规则
-  routes: []
+  routes: [
+    { path: '/login', name: 'login', component: Login }
+  ]
 })
 // 导出、挂载到main.js根实例下
 export default router
